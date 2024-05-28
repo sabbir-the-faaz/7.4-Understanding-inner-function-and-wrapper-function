2a9d1c86d2a6c9538f6e9ac5299c7a9812387ab5
Md. Sabbir Rahman Akash <161277809+the-faaz@users.noreply.github.com>
2024-05-28 07:12:59 +0600

    Add files via upload

---
+++ 7.4 Understanding inner function and wrapper function.py
@@ -0,0 +1,22 @@
+#fuction is a first class object
+def double_decker():
+  print('starting the double decker')
+  def inner_fun():
+    print('inside the inner')
+    return 3000
+  return inner_fun
+  
+#print(double_decker())
+#print(double_decker()())
+
+def do_something(work):
+  print('work started')
+  work()
+  print('work ended')
+  
+#do_something(2)
+
+def coding():
+  print('coding in python')
+
+do_something(coding)
